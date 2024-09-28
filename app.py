1dae473ca1d123e3ef6156d1c89df091ea1b4ee4
Juin28 <tanjuinlong@gmail.com>
2024-09-28 12:00:27 +0800

    juin

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import logging
 import socket
 
 from routes import app
+from flask import Flask, request, jsonify
+
+import routes.solve_the_wordle as solve_the_wordle
 
 logger = logging.getLogger(__name__)
 
@@ -9,6 +12,15 @@ logger = logging.getLogger(__name__)
 @app.route('/', methods=['GET'])
 def default_route():
     return 'Python Template'
+
+@app.route('/wordle-game', methods=['POST'])
+def wordle_game():
+    # data = request.json()
+    # print(data)
+    data = request.json
+    print(data)
+    guess = solve_the_wordle.make_guess(data)
+    return jsonify({"guess": guess})
 
 
 logger = logging.getLogger()
---
+++ routes/solve_the_wordle.py
@@ -0,0 +1,88 @@
+import random
+
+starting_word_list = ["soare", "roate", "raise"]
+
+global avail_guesses
+avail_guesses = "abcdefghijklmnopqrstuvwxyz"
+
+overall_avail_positions = [0, 1, 2, 3, 4]
+incorrect_positions = {}
+num_incorrect_positions = 0
+correct_positions = []
+guessed_characters = {
+    1 : {
+        "correct" : [],
+        "incorrect" : []
+    },
+    2 : {
+        "correct" : [],
+        "incorrect" : []
+    },
+    3 : {
+        "correct" : [],
+        "incorrect" : []
+    },
+    4 : {
+        "correct" : [],
+        "incorrect" : []
+    },
+    5 : {
+        "correct" : [],
+        "incorrect" : []
+    }
+}
+
+def update_knowledge(guess, evaluation):
+    global avail_guesses, num_incorrect_positions, correct_positions, overall_avail_positions, incorrect_positions
+    for i, (char, feedback) in enumerate(zip(guess, evaluation)):
+        if feedback == 'O':
+            # guessed_characters[i]["correct"].append(char)
+            guessed_characters[i]["correct"] = char
+            correct_positions.append(i)
+            overall_avail_positions.remove(i)
+        elif feedback == 'X':
+            if char not in incorrect_positions.keys():
+                incorrect_positions[char] = [i]
+            else:
+                incorrect_positions[char].append(i)
+            num_incorrect_positions += 1
+        elif feedback == '-':
+            avail_guesses = avail_guesses.replace(char, '')
+            
+def create_new_guess():
+    global avail_guesses, num_incorrect_positions, correct_positions, overall_avail_positions, incorrect_positions
+    new_guess = [None] * 5
+    
+    for i in range(5):
+        if i in correct_positions:
+            # new_guess[i] = guessed_characters[i]["correct"][0]
+            new_guess[i] = guessed_characters[i]["correct"]
+            
+        for index, guess in enumerate(new_guess):
+            if guess is None:
+                if num_incorrect_positions > 0:
+                    key = next(iter(incorrect_positions)) 
+                    wrong_positions = incorrect_positions[key]
+                    avail_positions = [pos for pos in range(5) if pos not in wrong_positions]
+                    avail_positions = [pos for pos in avail_positions if pos in overall_avail_positions]
+                    new_guess[avail_positions[0]] = key
+                    overall_avail_positions.remove(avail_positions[0])
+                    num_incorrect_positions -= 1
+                    incorrect_positions.pop(key)
+                else:
+                    new_guess[index] = random.choice(avail_guesses)
+            
+    return ''.join(new_guess)
+
+def make_guess(data):
+    guess_history = data.get('guessHistory', [])
+    evaluation_history = data.get('evaluationHistory', [])
+    
+    if not guess_history:
+        return random.choice(starting_word_list)
+
+    for guess, evaluation in zip(guess_history, evaluation_history):
+        update_knowledge(guess, evaluation)
+
+    new_guess = create_new_guess()
+    return new_guess
