7949de2a26e28786c6d80b0bad5c1bbb0c5d2395
Darren Chua <darrenchua314@gmail.com>
2024-09-28 14:52:19 +0800

    math colony!

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import socket
 from flask import request
 
-from routes import app, digital_colony, kazuma, solve_the_wordle, klotski, bugp2
+from routes import app, math_colony, kazuma, solve_the_wordle, klotski, bugp2
 from flask import Flask, request, jsonify
 
 
@@ -22,7 +22,7 @@ def wordle_game():
 @app.route('/digital-colony', methods=['POST'])
 def give_result():
     data = request.get_json()
-    return digital_colony.solution(data)
+    return math_colony.solution(data)
 
 @app.route('/efficient-hunter-kazuma', methods=['POST'])
 def give_kazuma_result():
