256f092d59915c5833d5c0a35aec8b0d052b9763
Juin28 <tanjuinlong@gmail.com>
2024-09-28 21:49:51 +0800

    dodge bullet

--- routes/dodge_bullet.py
+++ routes/dodge_bullet.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-# import copy
+import json
 
 bullets = {
     'u': [],
@@ -9,7 +9,6 @@ bullets = {
 }
 max_x = 0
 max_y = 0
-# player_position = (0, 0)
 
 """
 .dd
@@ -17,16 +16,25 @@ r*.
 ...
 """
 
+"""
+j0 j1 j2 j3 I0
+j0 j1 j2 j3 I1
+j0 j1 j2 j3 I2
+j0 j1 j2 j3 I3
+"""
 
 def parse_map(text_map):
     return [list(row) for row in text_map.strip().split('\\n')]
 
 def get_bullets_return_player_position(game_map):
     player_position= (0, 0)
-    for i in range(len(game_map)):
-        for j in range(len(game_map[0])):
+    # print(f"\n\n[DEBUG] max_y = {max_y}, max_x = {max_x}\n\n")
+    for i in range(max_y):
+        for j in range(max_x):
+            # print(f"\n\n[DEBUG] i = {i}, j = {j}\n\n")
             bullet = game_map[i][j]
-            if bullet in bullets.keys():
+            # if bullet in bullets.keys():
+            if bullet == 'u' or bullet == 'd' or bullet == 'l' or bullet == 'r':
                 bullets[bullet].append((j, i))
             elif bullet == '*':
                 player_position = (j, i)
@@ -49,56 +57,8 @@ def completely_safe(x, y):
                 
     return True
             
-    
-
-"""
-j0 j1 j2 j3 I0
-j0 j1 j2 j3 I1
-j0 j1 j2 j3 I2
-j0 j1 j2 j3 I3
-"""
-
-
-# def move_bullets(game_map):
-#     global bullets
-#     new_map = copy.deepcopy(game_map)
-#     # i = y, x = j
-#     for i in range(len(game_map)):
-#         for j in range(len(game_map[0])):
-#             for bullet_direction in game_map[i][j]:
-#                 if bullet_direction == 'u':
-#                     # if is_valid_move(game_map, j, i-1):
-#                     if 0 <= j < max_x and 0 <= i-1 < max_y:
-#                         new_map[i-1][j] += 'u'
-#                         bullets['u'].append((j, i-1))
-#                     new_map[i][j] = new_map[i][j].replace('u', '')
-#                     bullets['u'].remove((j, i))
-#                 elif bullet_direction == 'd':
-#                     # if is_valid_move(game_map, j, i+1):
-#                     if 0 <= j < max_x and 0 <= i+1 < max_y:
-#                         new_map[i+1][j] += 'd'
-#                         bullets['d'].append((j, i+1))
-#                     new_map[i][j] = new_map[i][j].replace('d', '')
-#                     bullets['d'].remove((j, i))
-#                 elif bullet_direction == 'l':
-#                     # if is_valid_move(game_map, j-1, i):
-#                     if 0 <= j-1 < max_x and 0 <= i < max_y:
-#                         new_map[i][j-1] += 'l'
-#                         bullets['l'].append((j-1, i))
-#                     new_map[i][j] = new_map[i][j].replace('l', '')
-#                     bullets['l'].remove((j, i))
-#                 elif bullet_direction == 'r':
-#                     # if is_valid_move(game_map, j+1, i):
-#                     if 0 <= j+1 < max_x and 0 <= i < max_y:
-#                         new_map[i][j+1] += 'r'
-#                         bullets['r'].append((j+1, i))
-#                     new_map[i][j] = new_map[i][j].replace('r', '')
-#                     bullets['r'].remove((j, i))
-    
-#     return new_map
 
 def move_bullets():
-    # print(f"\n\n[DEBUG] Enter move bullets\n\n")
     global bullets
     
     # Initialize a new map with empty strings
@@ -109,30 +69,32 @@ def move_bullets():
     for direction, positions in bullets.items():
         for bullet_x, bullet_y in positions:
             if direction == 'u' and 0 <= bullet_y - 1:
+                # print(f"\n\n[DEBUG] [U] bullet_x = {bullet_x}, bullet_y - 1= {bullet_y-1}, max_x = {max_x}, max_y = {max_y}\n\n")
                 new_map[bullet_y - 1][bullet_x] = new_map[bullet_y - 1][bullet_x].replace('.', '')
                 new_map[bullet_y - 1][bullet_x] += 'u'
                 new_bullets['u'].append((bullet_x, bullet_y - 1))
             elif direction == 'd' and bullet_y + 1 < max_y:
+                # print(f"\n\n[DEBUG] [D] bullet_x = {bullet_x}, bullet_y + 1 = {bullet_y+1}, max_x = {max_x}, max_y = {max_y}\n\n")
                 new_map[bullet_y + 1][bullet_x] = new_map[bullet_y + 1][bullet_x].replace('.', '')
                 new_map[bullet_y + 1][bullet_x] += 'd'
                 new_bullets['d'].append((bullet_x, bullet_y + 1))
             elif direction == 'l' and 0 <= bullet_x - 1:
+                # print(f"\n\n[DEBUG] [L] bullet_x - 1 = {bullet_x-1}, bullet_y = {bullet_y}, max_x = {max_x}, max_y = {max_y}\n\n")
                 new_map[bullet_y][bullet_x - 1] = new_map[bullet_y][bullet_x - 1].replace('.', '')
                 new_map[bullet_y][bullet_x - 1] += 'l'
                 new_bullets['l'].append((bullet_x - 1, bullet_y))
             elif direction == 'r' and bullet_x + 1 < max_x:
+                # print(f"\n\n[DEBUG] [R] bullet_x + 1 = {bullet_x+1}, bullet_y = {bullet_y}, max_x = {max_x}, max_y = {max_y}\n\n")
                 new_map[bullet_y][bullet_x + 1] = new_map[bullet_y][bullet_x + 1].replace('.', '')
                 new_map[bullet_y][bullet_x + 1] += 'r'
                 new_bullets['r'].append((bullet_x + 1, bullet_y))
     
-    # print(f"\n\n[DEBUG] bullets = {bullets} to new_bullets = {new_bullets}\n\n")
-    # print(f"\n\n[DEBUG] game_map = {game_map} to new_map = {new_map}\n\n")
     bullets = new_bullets
 
-    # print(f"\n\n[DEBUG] Exit move bullets\n\n")
     return new_map
 
 def find_safe_path(game_map, start_x, start_y):
+    print(f"\n[DEBUG] Enter find path\n")
     global bullets
     
     path = []
@@ -156,8 +118,10 @@ def find_safe_path(game_map, start_x, start_y):
         if has_valid_move:
             path.append(tmp_path)
         else:
+            print(f"\n[DEBUG] Exit find path as NONE\n")
             return None
     
+    print(f"\n[DEBUG] Exit find path\n")
     return path
 
 
@@ -172,10 +136,18 @@ def solution(request):
     start_x, start_y = player_pos
     safe_path = find_safe_path(game_map, start_x, start_y)
 
-    return jsonify({"instructions": safe_path})
+    # return jsonify({"instructions": safe_path})
+    print("\n[ANSWER]")
+    print({"instructions": safe_path})
+    return json.dumps({"instructions": safe_path})
     # return safe_path
 
 # if __name__ == "__main__":
 #     # test_input = "....\\n.r*.\\n...."
-#     test_input = "ddd\\nr*.\\n..."
+#     # test_input = "ddd\\nr*.\\n..."
+#     test_input = "d.d\\n.r*\\n.d."
 #     print(solution(test_input))
+    
+    
+    
+    
